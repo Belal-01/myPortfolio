473436ae9ff9e47e1c3b0dfa97dba2ce8207e080
Belal-01 <belalkhobieh343@gmail.com>
2024-08-20 23:24:12 +0300

    add tow projects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -16,12 +16,14 @@ const Projects = () => {
         <span className='title'>Recent Projects </span>
       </div>
       <div className="projects__gallery">
-        <Project img = {'GamesViewer'} title = {'Games Viewer'} info={'Responseive website Worked using React ,Sass, with use of Database (note:turn on vpn )'} url ={'https://games-viewer-reactapp.onrender.com'}/>
-        <Project img = {'Amazon'} title={'Amazon '} info={'Responseive website (Immitation of Amazon) Worked using JavaScript,HTML ,Css'} url ={'https://belal-01.github.io/Amazon-app/amazon.html'}/>
-        <Project img = {'NoteApp'} title={'Note App'} info={'Responseive website Worked using React ,Sass, with use of Database'} url={'https://note-app-react-app.onrender.com'}/>
-        <Project img = {'facebook'} title={'Facebook'} info={'Immitation of facebook worked using HTML , Sass '} url = {'https://belal-01.github.io/facebook-web/'}/>
-        <Project img = {'Tenzies'} title={'Tenzes Game'} info={'Worked using React ,impementation of state managment'} url = {'https://tenzies-react-app.onrender.com'}/>
-        <Project img = {'Portfolio'} title={'Portfolio Design'} info={'worked using javaScript,HTML,Sass,'} url = {'https://portfolio-grwg.onrender.com/'}/>
+         <Project img = {'portfolio2'} title={'My Portfolio'} info={'Responseive website Coded using React,Sass'} url = {'https://belalmustafa-portfolio-reactapp.onrender.com/'}/>
+        <Project img = {'GamesViewer'} title = {'Games Viewer'} info={'Responseive website Coded using React ,Sass, with use of Database (note:turn on vpn )'} url ={'https://games-viewer-reactapp.onrender.com'}/>
+        <Project img = {'Amazon'} title={'Amazon '} info={'Responseive website (Immitation of Amazon) Coded using JavaScript,HTML ,Css'} url ={'https://belal-01.github.io/Amazon-app/amazon.html'}/>
+        <Project img = {'NoteApp'} title={'Note App'} info={'Responseive website Coded using React ,Sass, with use of Database'} url={'https://note-app-react-app.onrender.com'}/>
+        <Project img = {'facebook'} title={'Facebook'} info={'Immitation of facebook Coded using HTML , Sass '} url = {'https://belal-01.github.io/facebook-web/'}/>
+        <Project img = {'Tenzies'} title={'Tenzes Game'} info={'Coded using React ,impementation of state managment'} url = {'https://tenzies-react-app.onrender.com'}/>
+        <Project img = {'youtube'} title={'Youtube Design'} info={'Coded using ,HTML,CSS'} url = {'https://belal-01.github.io/youtube/'}/>
+        <Project img = {'Portfolio'} title={'Portfolio Design'} info={'Coded using javaScript,HTML,Sass,'} url = {'https://portfolio-grwg.onrender.com/'}/>
 
 
       </div>
