f986ae2bacd6935b48042d518362d6dd00d4710b
Belal-01 <belalkhobieh343@gmail.com>
2024-08-23 07:58:49 +0300

    remove youtube and faceBook projects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -18,9 +18,9 @@ const Projects = ({anim}) => {
         <Project anim={anim} img = {'GamesViewer'} title = {'Games Viewer'} info={'Responseive website Coded using React ,Sass, with use of Database (Note:Turn on VPN)'} url ={'https://games-viewer-reactapp.onrender.com'}/>
         <Project anim={anim} img = {'Amazon'} title={'Amazon '} info={'Responseive website Coded using JavaScript,HTML ,Css'} url ={'https://belal-01.github.io/Amazon-app/amazon.html'}/>
         <Project anim={anim} img = {'NoteApp'} title={'Note App'} info={'Responseive website Coded using React ,Sass, with use of Database (Note:Turn on VPN)'} url={'https://note-app-react-app.onrender.com'}/>
-        <Project anim={anim} img = {'facebook'} title={'Facebook'} info={'Main page of Facebook Coded using HTML , Sass '} url = {'https://belal-01.github.io/facebook-web/'}/>
+        {/* <Project anim={anim} img = {'facebook'} title={'Facebook'} info={'Main page of Facebook Coded using HTML , Sass '} url = {'https://belal-01.github.io/facebook-web/'}/> */}
         <Project anim={anim} img = {'Tenzies'} title={'Tenzes Game'} info={'Coded using React ,implementation of state managment'} url = {'https://tenzies-react-app.onrender.com'}/>
-        <Project anim={anim} img = {'youtube'} title={'Youtube Design'} info={'Main page of Youtube  Coded using ,HTML,CSS'} url = {'https://belal-01.github.io/youtube/'}/>
+        {/* <Project anim={anim} img = {'youtube'} title={'Youtube Design'} info={'Main page of Youtube  Coded using ,HTML,CSS'} url = {'https://belal-01.github.io/youtube/'}/> */}
         <Project anim={anim} img = {'Portfolio'} title={'Portfolio Design'} info={'Coded using javaScript,HTML,Sass,'} url = {'https://portfolio-grwg.onrender.com/'}/>
 
 
